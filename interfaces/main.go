056d224b82fa74040a6c8ab54bc3ef05da5a5076
VEDVRAT SHARMA <vedvrat@indebted.io>
2019-02-04 15:25:18 +1100

    Interfaces

--- interfaces/main.go
+++ interfaces/main.go
@@ -1,7 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
 
+// Learn Interfaces
 type bot interface {
 	greeting() string
 }
@@ -10,11 +16,18 @@ type englishBot struct{}
 type spanishBot struct{}
 
 func main() {
-	eb := englishBot{}
-	sb := spanishBot{}
+	// eb := englishBot{}
+	// sb := spanishBot{}
 
-	printGreeting(eb)
-	printGreeting(sb)
+	// printGreeting(eb)
+	// printGreeting(sb)
+
+	resp, err := http.Get("http://google.com")
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	io.Copy(os.Stdout, resp.Body)
 }
 
 func printGreeting(b bot) {
